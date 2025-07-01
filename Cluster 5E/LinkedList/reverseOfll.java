0c15f61582e571f34b54fec90a4742a32e1b124a
Tanmay Singh Thakur <itsthakurtanmay@gmail.com>
2025-07-01 09:57:43 +0530

    01-07-2025

---
+++ Cluster 5E/LinkedList/reverseOfll.java
@@ -0,0 +1,16 @@
+
+class reverseOfll {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode forward = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+
+    }
+}
